390acdbff555cfc22ba3ddce4de1c8c91257a754
Vitali Shvaichuk <vit.shvaichuk@gmail.com>
2025-05-24 19:28:17 +0300

    feat: add put method

--- src/main/java/by/fstk1337/datatype/HashMap.java
+++ src/main/java/by/fstk1337/datatype/HashMap.java
@@ -1,6 +1,7 @@
 package by.fstk1337.datatype;
 
 import java.util.Arrays;
+import java.util.Objects;
 
 public class HashMap<K, V> {
     private static final int DEFAULT_CAPACITY = 16;
@@ -18,12 +19,45 @@ public class HashMap<K, V> {
         hashTable = new Entry[capacity];
     }
 
+    private static int index(Object key, int capacity) {
+        int hash = key.hashCode();
+        return hash & (capacity - 1);
+    }
+
     public int size() {
         return size;
     }
 
     public boolean isEmpty() {
         return size == 0;
+    }
+
+    public V put(K key, V value) {
+        int index = index(key, capacity);
+        Entry<K, V> entry = hashTable[index];
+        if (Objects.isNull(entry)) {
+            hashTable[index] = new Entry<>(index, key, value, null);
+            size += 1;
+            return null;
+        } else {
+            while (entry.hasNext()) {
+                if (entry.getKey().equals(key)) {
+                    V oldValue = entry.getValue();
+                    entry.setValue(value);
+                    size +=1;
+                    return oldValue;
+                }
+                entry = entry.getNext();
+            }
+            if (entry.getKey().equals(key)) {
+                V oldValue = entry.getValue();
+                entry.setValue(value);
+                size += 1;
+                return oldValue;
+            }
+            entry.setNext(new Entry<>(index, key, value, null));
+        }
+        return null;
     }
 
     @Override
--- src/main/java/by/fstk1337/datatype/Main.java
+++ src/main/java/by/fstk1337/datatype/Main.java
@@ -2,16 +2,15 @@ package by.fstk1337.datatype;
 
 public class Main {
     public static void main(String[] args) {
-        HashMap<String, String> map1 = new HashMap<>(5);
-        HashMap<String, String> map2 = new HashMap<>();
+        HashMap<String, String> map = new HashMap<>(3);
 
-        System.out.println(map1);
-        System.out.println(map2);
+        map.put("Adam", "The One");
+        map.put("Peter", "The Great");
+        map.put("Jamie", "The Unstoppable");
+        map.put("Adam", "Goat 777");
+        map.put("Jacky", "High jumper");
 
-        System.out.println(map1.size());
-        System.out.println(map1.isEmpty());
-
-        System.out.println(map2.size());
-        System.out.println(map2.isEmpty());
+        System.out.println(map);
+        System.out.println(map.size());
     }
 }
